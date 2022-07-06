26863f82dd201bdeb4a32e5de45fc2a8baee4462
tia01017 <mattia.lecchi@protonmail.com>
2022-07-06 09:52:54 +0200

    Riorganizzata struttura directory

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 const express = require('express')
 const path = require('path')
-const ejs = require('ejs')
+// const ejs = require('ejs')
 const fs =  require('fs')
 
 const app = express()
 
 app.use(express.static('public'))
-let head = fs.readFileSync('public/head.html', 'utf8')
-publicpath = {root: path.join(__dirname, 'public')}
 
 // home
 app.get('/', (req, res) => {
-    ejs.renderFile('public/index.ejs', {head:head}, (err, str) => {
-        res.send(str)
-    })
+   res.render('index.ejs')
 })
 
 // login
 app.get('/login', (req, res) => {
-    ejs.renderFile('public/login.ejs', {head:head}, (err, str) => {
-        res.send(str)
-    })
+    res.render('login.ejs')
+})
+
+// pagina dell'account
+app.get('/me', (req,res) => {
+    res.render('mypage.ejs')
 })
 
 // pagina inesistente
